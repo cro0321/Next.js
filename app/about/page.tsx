f935aa201d643ae2d9b24b57e6e6fd55a1de4b24
박은성 <cro3179@naver.com>
2023-11-02 16:05:56 +0900

    프로필 수정

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,7 +7,7 @@ export default function About() {
         <>
             <div className="w-full pt-12 mt-16 px-[2%] pb-8 ">
                 <div className="max-w-7xl mx-auto p-5 bg-white rounded-md border dark:bg-[#272929] dark:text-[#ebf4f1]">
-                   <h3 className="text-center font-bold sm:text-2xl lg:text-3xl">알뜰요금제로 갈아타기</h3>
+                   <h3 className="text-center font-bold sm:text-2xl lg:text-3xl">프로필</h3>
                 </div>
                 <div className="max-w-7xl mx-auto">
                     <div className="h-auto flex overflow-hidden mt-8 flex-wrap justify-between">
